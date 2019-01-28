d47925e7a8f95f0e97fa9d9612ee2c25d6744357
Marcin <mpx186@gmail.com>
2019-01-28 12:32:28 +0100

    Updates

---
+++ precache-manifest.b792cea193dc557b765f944826a68d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b69ef19bc2e0ae7e2d6a270cd2d35b7",
+    "url": "/weather-app/static/media/background.8b69ef19.png"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "13eca78b8f38d1ab947e",
+    "url": "/weather-app/static/js/main.13eca78b.chunk.js"
+  },
+  {
+    "revision": "8b6de34057951420861c",
+    "url": "/weather-app/static/js/1.8b6de340.chunk.js"
+  },
+  {
+    "revision": "13eca78b8f38d1ab947e",
+    "url": "/weather-app/static/css/main.d92a9d5a.chunk.css"
+  },
+  {
+    "revision": "e0a4cf1ee9f94733793e20a91655474b",
+    "url": "/weather-app/index.html"
+  }
+];
